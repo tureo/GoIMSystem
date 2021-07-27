567b93bfa42fe535d7ae765390102058d11352f1
tureo <dawushi@hotmail.com>
2021-07-27 18:38:29 +0800

    Client menu display

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int // 当前client的模式
 }
 
 func NewClient(serverIP string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIP string, serverPort int) *Client {
 	client := &Client{
 		ServerIP:   serverIP,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 
 	// 连接server
@@ -30,6 +32,47 @@ func NewClient(serverIP string, serverPort int) *Client {
 
 	// 返回对象
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>请输入合法范围内的数字<<<")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+
+		}
+
+		// 根据不通的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("公聊模式选择...")
+		case 2:
+			// 私聊模式
+			fmt.Println("私聊模式选择...")
+		case 3:
+			// 更新用户名
+			fmt.Println("更新用户名选择...")
+
+		}
+	}
 }
 
 var serverIP string
@@ -54,5 +97,5 @@ func main() {
 	fmt.Println(">>>连接服务器成功...")
 
 	// 启动客户端的业务
-	select {}
+	client.Run()
 }
