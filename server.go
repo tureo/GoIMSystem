7ec82c9c8a992e33d217fd139a8c7c110553b4d9
tureo <dawushi@hotmail.com>
2021-07-27 16:14:23 +0800

    BroadCast User Message

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -63,6 +64,30 @@ func (s *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线消息
 	s.BroadCast(user, "已上线")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				s.BroadCast(user, "下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("conn read err: ", err)
+				return
+			}
+
+			// 读取用户的消息，去掉"\n"
+			msg := string(buf[:n-1])
+
+			// 将得到的消息进行广播
+			s.BroadCast(user, msg)
+		}
+
+	}()
 
 	// 当前handler阻塞
 	select {}
