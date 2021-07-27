b952cc00842d9d3a7baa56183812a51d6f6c116e
tureo <dawushi@hotmail.com>
2021-07-27 17:21:09 +0800

    Session timeout forced close

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -59,6 +60,9 @@ func (s *Server) Handler(conn net.Conn) {
 
 	user.Online()
 
+	// 监听是否活跃的channel
+	isLive := make(chan bool)
+
 	// 接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -79,12 +83,35 @@ func (s *Server) Handler(conn net.Conn) {
 
 			// 用户针对msg进行消息处理
 			user.DoMessage(msg)
+
+			// 用户的任意消息，代表当前用户是活跃的
+			isLive <- true
 		}
 
 	}()
 
 	// 当前handler阻塞
-	select {}
+	for {
+		select {
+		case <-isLive:
+			// 当前用户是活跃的，应该重置定时器
+			// 不做任何事情，为了激活select，更新下面的定时器
+
+		case <-time.After(time.Second * 10):
+			// 已经超时
+			// 将当前的user强制关闭
+			user.SendMsg("你被踢了")
+
+			// 销毁使用的相关资源
+			close(user.C)
+
+			// 关闭连接
+			conn.Close()
+
+			// 退出当前的handler
+			return // runtime.Goexit()
+		}
+	}
 }
 
 // 启动服务器的接口
@@ -113,5 +140,4 @@ func (s *Server) Start() {
 		// dohandler
 		go s.Handler(conn)
 	}
-
 }
